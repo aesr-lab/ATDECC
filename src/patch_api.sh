26a55675fb4d49d4ff0948b9b7012d685f3a1fc2
Thomas Grill <gr@grrrr.org>
2023-03-19 01:30:47 +0100

    Simplified patch script

--- src/patch_api.sh
+++ src/patch_api.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-tmpfile=$(mktemp)
+function patch {
+	tmpfile=$(mktemp)
+	cp "${1}" "$tmpfile"
+	sed -e "s/${2}/${3}/g" "$tmpfile" > "$1"
+	rm -f "$tmpfile"
+}
 
-cp "$1" "$tmpfile"
-sed -e 's/^.*0().*$/#&/g' "$tmpfile" > "$1"
+# comment out syntactically wrong type definitions ending with ' * 0()'
+patch "${1}" '^.*[[:space:]]\*[[:space:]]0().*$' '#&'
 
-cp "$1" "$tmpfile"
-sed -e 's/^uint16_t = .*$/uint16_t = ctypes.c_uint16/g' "$tmpfile" > "$1"
+# patch buggy uint16_t type definition
+patch "${1}" '^uint16_t = .*$' 'uint16_t = ctypes.c_uint16'
 
-cp "$1" "$tmpfile"
-sed -e 's/^const_string_t = .*$/const_string_t = ctypes.POINTER(ctypes.c_char)/g' "$tmpfile" > "$1"
-
-rm -f "$tmpfile"
-
+# patch const_string_t type definition which varies across OSs   
+patch "${1}" '^const_string_t = .*$' 'const_string_t = ctypes.POINTER(ctypes.c_char)'
