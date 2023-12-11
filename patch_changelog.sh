51e624f446463bd79867956c384591b20ccb4475
Thomas Grill <thomas@rottingsounds.org>
2023-12-11 22:11:43 +0100

    Small fixes

--- patch_changelog.sh
+++ patch_changelog.sh
@@ -4,12 +4,8 @@
 # $2 ... DEST file
 
 if [ -z "$VERSION" ]; then
-        if [ -f .version ]; then
-                VERSION=$(cat .version)
-        else
-                VERSION="0.01-0"
-                echo "No version given: assuming $VERSION" > /dev/stderr
-        fi
+	VERSION="0.01-0"
+	echo "No version given: assuming $VERSION" > /dev/stderr
 fi
 
 ver_norev=${VERSION%-*}
@@ -26,7 +22,7 @@ if [ -z "$ver_norev" ] || [ -z "$rev" ]; then
 fi
 
 # Writing new revision number to .version file
-echo $ver_norev-$((rev+1)) > .version
+VERSION="$ver_norev-$rev" 
 
 if [ -z "$DATE" ]; then
         DATE=$(LANG= date +"%a, %d %b %Y %H:%I:%S %z")
