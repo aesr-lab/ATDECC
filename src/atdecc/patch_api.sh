12ed304f23dde4d40bdf23cf1c0ffe6f49976661
Thomas Grill <thomas@rottingsounds.org>
2023-12-26 01:28:33 +0100

    Minimum addition to PR

--- src/atdecc/patch_api.sh
+++ src/atdecc/patch_api.sh
@@ -13,13 +13,13 @@ function sed_patch {
 }
 
 # Comment out syntactically wrong type definitions ending with ' * 0()'
-sed_patch "${file}" '^.*[[:space:]]\*[[:space:]]0().*$' '#&'
+sed_patch "${file}" '^.*[[:space:]]\*[[:space:]]0().*$' '#&' &&
 
 # Patch buggy uint16_t type definition
-sed_patch "${file}" '^uint16_t = .*$' 'uint16_t = ctypes.c_uint16'
+sed_patch "${file}" '^uint16_t = .*$' 'uint16_t = ctypes.c_uint16' &&
 
 # Patch const_string_t type definition which varies across OSs   
-sed_patch "${file}" '^const_string_t = .*$' 'const_string_t = ctypes.POINTER(ctypes.c_char)'
+sed_patch "${file}" '^const_string_t = .*$' 'const_string_t = ctypes.POINTER(ctypes.c_char)' &&
 
 # Patch in runtime path of shared library ( __file__.rsplit("/",1)[0] )
 sed_patch "${file}" 'ctypes.CDLL(\(.*\))$' 'ctypes.CDLL(__file__\.rsplit(\"\/\"\,1)[0]+\"\/\"+\1)'
